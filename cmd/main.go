ecd4fff8648f6c60cfc2ba3bc598977598ac7b34
sebashmakov <sebashmakov@ozon.ru>
2022-07-19 23:42:41 +0300

    init main

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Hello from a HandleFunc #1!\n")
+	}
+
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8085", nil))
+}
